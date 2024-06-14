28301b53a202dfb1eb887b8f3cce2fb2a535fac1
pedroazeredo04 <pedro_azeredo@usp.br>
2024-06-14 07:57:38 -0300

    🎨 Format code

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,40 @@
+#include <pinout.hpp>
 #include <task/blinky.hpp>
 #include <task/state_machine_task.hpp>
-#include <pinout.hpp>
 #include <task_scheme.hpp>
 const uint32_t blinky_frequency_ms{500};
 // Tasks initializations
- rfidoor::task::BlinkyTask blinky_task(rfidoor::pinout::board_led, blinky_frequency_ms, rfidoor::task::blinky_config);
- rfidoor::task::StateMachineTask state_machine_task(rfidoor::task::state_machine_config);
+rfidoor::task::BlinkyTask blinky_task(rfidoor::pinout::board_led,
+                                      blinky_frequency_ms,
+                                      rfidoor::task::blinky_config);
+rfidoor::task::StateMachineTask
+    state_machine_task(rfidoor::task::state_machine_config);
 
 void setup() {
   blinky_task.create_task();
   state_machine_task.create_task();
 
   rfidoor::pinout::lcd.init();
-  rfidoor::pinout::lcd.set_cursor(0,0);
-	rfidoor::pinout::lcd.write("Hello, world!");
-	rfidoor::pinout::lcd.set_cursor(2,1);
-	rfidoor::pinout::lcd.write("Chupa Tsuzuki");
-  rfidoor::pinout::lcd.write_special_char(rfidoor::peripheral::SKULL_SPECIAL_CHAR);
+  rfidoor::pinout::lcd.set_cursor(0, 0);
+  rfidoor::pinout::lcd.write("Hello, world!");
+  rfidoor::pinout::lcd.set_cursor(2, 1);
+  rfidoor::pinout::lcd.write("Chupa Tsuzuki");
+  rfidoor::pinout::lcd.write_special_char(
+      rfidoor::peripheral::SKULL_SPECIAL_CHAR);
 
   delay(5000);
   rfidoor::pinout::lcd.clear();
   rfidoor::pinout::lcd.set_cursor(0, 0);
   rfidoor::pinout::lcd.write("Digite a senha ");
-  rfidoor::pinout::lcd.write_special_char(rfidoor::peripheral::LOCK_SPECIAL_CHAR);
+  rfidoor::pinout::lcd.write_special_char(
+      rfidoor::peripheral::LOCK_SPECIAL_CHAR);
   rfidoor::pinout::lcd.set_cursor(0, 1);
 }
 
 void loop() {
   char key = rfidoor::pinout::keyboard.getKey();
 
-  if (key){
+  if (key) {
     rfidoor::pinout::lcd.write_char_with_increment(key);
   }
 }
