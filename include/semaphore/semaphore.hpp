87426627bc3d0b477d16afad7d7eaba08918e37e
lucasmarques2003 <lucas-marques2003@usp.br>
2024-06-16 05:25:34 -0300

    ✨ Add semaphore generic class

---
+++ include/semaphore/semaphore.hpp
@@ -0,0 +1,51 @@
+/**
+ * @file semaphore.hpp
+ * 
+ * @brief Generic semaphore class header
+ * 
+ * @date 06/2024
+ */
+
+#ifndef __SEMAPHORE_HPP__
+#define __SEMAPHORE_HPP__
+
+#include <Arduino.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/semphr.h>
+
+namespace rfidoor::semaphore {
+
+class Semaphore {
+public:
+  /**
+   * @brief Constructor for the generic Semaphore class
+   */
+  Semaphore(void);
+
+  /**
+   * @brief Create semaphore function to be overriden by the derived class
+   * 
+   * @return SemaphoreHandle_t Handle for the semaphore
+   */
+  virtual SemaphoreHandle_t create_semaphore() = 0;
+
+  /**
+   * @brief Function that calls xSemaphoreGive from FreeRTOS
+   */
+  const void give_semaphore();
+
+  /**
+   * @brief Function that calls xSemaphoreTake from FreeRTOS
+   */
+  const void take_semaphore();
+
+private:
+    /**
+     * @brief Handle for the semaphore, required by xSemaphoreCreateBinary RTOS function
+     */
+    SemaphoreHandle_t semaphore_handle;
+};
+    
+} // namespace rfidoor::semaphore
+
+#endif // __SEMAPHORE_HPP__
---
+++ src/semaphore/semaphore.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file semaphore.cpp
+ * 
+ * @brief Generic semaphore class implementation
+ * 
+ * @date 06/2024
+ */
+
+#include "semaphore/semaphore.hpp"
+
+namespace rfidoor::semaphore {
+
+Semaphore::Semaphore(void) {
+    this->semaphore_handle = this->create_semaphore();
+}
+
+const void Semaphore::give_semaphore() {
+    xSemaphoreGive(this->semaphore_handle);
+}
+
+const void Semaphore::take_semaphore() {
+    xSemaphoreTake(this->semaphore_handle, portMAX_DELAY);
+}
+
+}
