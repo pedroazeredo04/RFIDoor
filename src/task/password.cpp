20bec997c200c8eadae5f61821e3ffae5a11e0b0
lucasmarques2003 <lucas-marques2003@usp.br>
2024-06-17 17:15:56 -0300

    🐛 Add event publishing for password input in PasswordTask

--- src/task/password.cpp
+++ src/task/password.cpp
@@ -52,6 +52,7 @@ void PasswordTask::read_password() {
 
     if (key >= '0' and key <= '9') {
       this->current_password += key;
+      rfidoor::queue::blackboard::event_queue.publish(event_t::TECLA);
     }
 
     if ((this->current_password.length() >= password_max_length) or
